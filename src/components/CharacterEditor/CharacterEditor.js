53307c7c667f9448826e24694daa7b3eb4a15475
Maryna Zadoianchuk <marina.zadoyanchuk@gmail.com>
2022-12-07 17:20:10 +0100

    exercises 1-3

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -77,8 +77,8 @@ function App() {
           />
         </div>
       </MaxWidthWrapper>
-
       <div className={styles.characterWrapper}>
+
         <Character
           body={body}
           head={head}
